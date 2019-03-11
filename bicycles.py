e73f1de6a29982ec21c1e464225e6471f35476b5
malinroot <kurchevskijpavel@gmail.com>
2019-03-11 17:13:28 +0300

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,13 @@
+# List of bike models
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
