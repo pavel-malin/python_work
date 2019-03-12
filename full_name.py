968043b8773fb790510ff61eafeedbf822e2c5bc
malinroot <kurchevskijpavel@gmail.com>
2019-03-12 23:56:01 +0300

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,19 @@
+# Concatenation
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# Capitalization Concatenation
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+
+# Concatenation  due(message)
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
