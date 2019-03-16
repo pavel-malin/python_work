99d905acd778159c6a1ead674e2a8eb2d8d72411
malinroot <kurchevskijpavel@gmail.com>
2019-03-16 17:50:14 +0300

    Add files via upload

---
+++ simple_messages.py
@@ -0,0 +1,20 @@
+# Assignment one(Hello world is messages), output but
+# different message
+simple_messages = "Hello world is messages"
+print(simple_messages)
+
+# Assignment one(Hello)
+simple_messages = "Hello"
+print(simple_messages)
+
+# Assignment one(world)
+simple_messages = "world"
+print(simple_messages)
+
+# Assignment one(is)
+simple_messages = "is"
+print(simple_messages)
+
+# Assingnment one(messages)
+simple_messages = "messages"
+print(simple_messages)
