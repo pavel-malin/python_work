2fe4e2216ce9e6b267c490f457dbe210df681e2a
malinroot <kurchevskijpavel@gmail.com>
2019-03-16 17:49:01 +0300

    Add files via upload

---
+++ see_world.py
@@ -0,0 +1,31 @@
+# see word
+see_world = ['Moscwo', 'Minsk', 'Grodno', 'Krakow', 'Piter']
+print(see_world)
+
+# Alphabe order(see_world list)
+print(sorted(see_world))
+
+# Former form
+print(see_world)
+
+# Alphabetical order from the end
+print(sorted(see_world, reverse=True))
+
+# Former form
+print(see_world)
+
+# Alphabetical order from the end
+see_world.reverse()
+print(see_world)
+
+# Fromer form
+see_world.reverse()
+print(see_world)
+
+# Alphabe order(see_world list)
+see_world.sort()
+print(see_world)
+
+# Alphabetical order from the end
+see_world.sort(reverse=True)
+print(see_world)
