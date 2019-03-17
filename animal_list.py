cc8d1886d707378dbc37e948c6b265101778ec2d
malinroot <kurchevskijpavel@gmail.com>
2019-03-17 23:28:26 +0300

    Add files via upload

---
+++ animal_list.py
@@ -0,0 +1,14 @@
+# List animal
+animal_list = ['dog', 'cat', 'hamster', 'parrot']
+for animal_lists in animal_list:
+    print(animal_lists)
+
+# List animals with message added
+animal_list = ['dog', 'cat', 'hamster', 'parrot']
+for animal_lists in animal_list:
+    print("A dog would make a great pet " + animal_lists)
+
+# Listing animals with a message at the beginning and at the end
+animal_list = ['dog', 'cat', 'hamster', 'parrot']
+for animal_lists in animal_list:
+    print("A dog would make a great pet " + animal_lists + "." + " Any of these animals would make a great pet!")
