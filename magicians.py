e4d6e86de342b666e1ad721986a1fe950894f7ad
malinroot <kurchevskijpavel@gmail.com>
2019-03-18 16:48:50 +0300

    Add files via upload

---
+++ magicians.py
@@ -0,0 +1,31 @@
+# Conclusion, names of magicians
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+# Cunclusion, names of magicians with appeal. With thanks
+magician = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.
+        title() + ".\n")
+
+
+print("Thank you, everyone. That was a great magic show!")
+
+# Cunclusion, names of magicians with appeal.
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+print("I can't wait to see your next trick," + magician.title() + 
+    ".\n")
+
+
+# Thanks to every magician
+magician = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+    print("Thank you everyone, that was a great magic show!")
+
