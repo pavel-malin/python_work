1d243865644f414fecbe39eb26beb8047cc6e7bc
malinroot <kurchevskijpavel@gmail.com>
2019-03-20 13:06:51 +0300

    Add files via upload

---
+++ pizzy_name.py
@@ -0,0 +1,20 @@
+# List one pizza name
+pizzy_name = ['margarita', 'diabola', 'calzone']
+for pizzy_names in pizzy_name:
+    print(pizzy_names)
+
+# Before each message name
+pizzy_name = ['margarita', 'diabola', 'calzone']
+for pizzy_names in pizzy_name:
+    print("I like pepperoni pizza " + pizzy_names.title())
+
+# Two message posts before each pizza name
+pizzy_name = ['margarita', 'diabola', 'calzone']
+for pizzy_names in pizzy_name:
+    print("I like pepperoni pizza " + pizzy_names.title() + 
+       " I really love pizza! " + pizzy_names.title())
+
+# Dispay message in front of pizza name
+pizzy_name = ['margarita', 'diabola', 'calzone']
+for pizzy_names in pizzy_name:
+    print("I like pepperoni pizza " + pizzy_names.title() + ".")
