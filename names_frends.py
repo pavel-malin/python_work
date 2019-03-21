2bbb1d3e1a401891d4c398a336cc9794a52cedd0
malinroot <kurchevskijpavel@gmail.com>
2019-03-21 23:25:56 +0300

    Add files via upload

---
+++ names_frends.py
@@ -0,0 +1,22 @@
+# List of names of friends. Index query
+names = ['Dens', 'Amir', 'Wind']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[-1])
+print(names[-2])
+print(names[-3])
+
+# Capitalized names
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+# Capitalization of the speaker
+auto = ['bmw', 'audi', 'nisssan']
+message = 'I would like to a car '
+print(message.title() + auto[0].title())
+print(message.title() + auto[1].title())
+print(message.title() + auto[2].title())
+
+
