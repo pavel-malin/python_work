4cb5fb27a8ce3cf6821f89de770c87acee57b17e
malinroot <kurchevskijpavel@gmail.com>
2019-03-21 23:24:10 +0300

    Add files via upload

---
+++ dimensions.py
@@ -0,0 +1,20 @@
+# Tuple
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+# single output tuple
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
+
+# New tuple
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for deminsion in dimensions:
+    print(deminsion)
