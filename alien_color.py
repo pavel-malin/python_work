8afe2622b772f3f0b0e7fb93c95b8baafde44f8e
malinroot <kurchevskijpavel@gmail.com>
2019-03-24 23:09:32 +0300

    Add files via upload

---
+++ alien_color.py
@@ -0,0 +1,177 @@
+# Getting points for color (green)
+alien_color = ['red', 'green', 'yellow']
+
+if 'green' in alien_color:
+    print("5")
+
+# Getting points for color (green)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    print("6")
+elif 'blue' in alien_color:
+    print("9")
+elif 'green' in alien_color:
+    print("1")
+else:
+    print("not")
+
+# Getting points for color (red, green, yellow)
+alien_color = ['red', 'green', 'yellow']
+
+if 'red' in alien_color:
+    print("2")
+if 'green' in alien_color:
+    print("6")
+if 'yellow' in alien_color:
+    print("9")
+
+# Getting points for color (not color)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    print("1")
+elif 'while' in alien_color:
+    print("2")
+elif 'blue' in alien_color:
+    print("3")
+else:
+    print("not message")
+
+# Getting points for color (green)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    print("1")
+elif 'while' in alien_color:
+    print("2")
+elif 'green' in alien_color:
+    print("5")
+
+# Getting points for color (green)
+alien_color = ['red' , 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 2
+elif 'green' in alien_color:
+    price = 3
+
+print("nice " + str(price))
+
+# Getting points for color (green)
+alien_color = ['red', 'green', 'yellow']
+
+if 'while' in alien_color:
+    price = 2
+elif 'black' in alien_color:
+    price = 1
+elif 'green' in alien_color:
+    price = 5
+elif 'blue' in alien_color:
+    price = 4
+print("nice " + str(price))
+
+# Getting points for color (red)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'red' in alien_color:
+    price = 10
+elif 'blue' in alien_color:
+    price = 2
+print("nice " + str(price))
+
+# Getting points for color (not color)
+alien_color = ['red' 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 2
+elif 'while' in alien_color:
+    price = 3
+else:
+    print("not price")
+
+# Getting points for color (yellow)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 4
+elif 'yellow' in alien_color:
+    price = 10
+else:
+    print("not price")
+print("nice " + str(price))
+
+
+# Getting points for color (red)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 2
+elif 'red' in alien_color:
+    price = 15
+print("nice " + str(price))
+
+# Getting points for color (red)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 2
+elif 'red' in alien_color:
+    price = 15
+print("nice " + str(price))
+
+# Getting points for color (green)
+alien_color = ['red', 'green', 'yellow']
+
+if 'while' in alien_color:
+    price = 2
+elif 'black' in alien_color:
+    price = 1
+elif 'green' in alien_color:
+    price = 5
+print("nice " + str(price))
+
+# Getting points for color (yellow)
+alien_color = ['red', 'green', 'yellow']
+
+if 'while' in alien_color:
+    price = 2
+elif 'black' in alien_color:
+    price = 1
+elif 'yellow' in alien_color:
+    price = 10
+print("nice " + str(price))
+
+# Getting points for color (red)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 2
+elif 'red' in alien_color:
+    price = 15
+print("nice " + str(price))
+
+# Getting points for color (not color)
+alien_color = ['red', 'green', 'yellow']
+
+if 'black' in alien_color:
+    price = 1
+elif 'while' in alien_color:
+    price = 2
+elif 'blue' in alien_color:
+    price = 3
+else:
+    print("not nice")
+
