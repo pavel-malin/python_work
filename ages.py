0c6544b92373c54507b38f8185334935083b7312
malinroot <kurchevskijpavel@gmail.com>
2019-03-24 23:09:03 +0300

    Add files via upload

---
+++ ages.py
@@ -0,0 +1,84 @@
+# What category is a person's age (12)
+age = 12
+
+if age < 2:
+    price = 'baby'
+elif age >= 2 and age < 4:
+    price = 'kid'
+elif age >= 4 and age < 13:
+    price = 'child'
+elif age >= 13 and age < 20:
+    price = 'teenager'
+elif age >= 20 and age < 65:
+    price = 'adult'
+else:
+    price = 'old man'
+print("hi " + str(price))
+
+# What category is a person's age (1)
+age = 1
+
+if age < 2:
+    price = 'baby'
+elif age >= 2 and age < 4:
+    price = 'kid'
+elif age >= 4 and age < 13:
+    price = 'child'
+elif age >= 13 and age < 20:
+    price = 'teenager'
+elif age >= 20 and age < 65:
+    price = 'adult'
+else:
+    price = 'old man'
+print("hi " + str(price))
+
+# What category is a person's age (18)
+age = 18
+
+if age < 2:
+    price = 'baby'
+elif age >= 2 and age < 4:
+    price = 'kid'
+elif age >= 4 and age < 13:
+    price = 'child'
+elif age >= 13 and age < 20:
+    price = 'teenager'
+elif age >= 20 and age < 65:
+    price = 'adult'
+else:
+    price = 'old man'
+print("hi " + str(price))
+
+# What category is a person's age (20)
+age = 20
+
+if age < 2:
+    price = 'baby'
+elif age >= 2 and age < 4:
+    price = 'kid'
+elif age >= 4 and age < 13:
+    price = 'child'
+elif age >= 13 and age < 20:
+    price = 'teenager'
+elif age >= 20 and age < 65:
+    price = 'adult'
+else:
+    price = 'old man'
+print("hi " + str(price))
+
+# What category is a person's age (75)
+age = 75
+
+if  age < 2:
+    price = 'baby'
+elif age >= 2 and age < 4:
+    price = 'kid'
+elif age >= 4 and age < 13:
+    price = 'child'
+elif age >= 13 and age < 20:
+    price = 'teenager'
+elif age >= 20 and age < 65:
+    price = 'adult'
+else:
+    price = 'old man'
+print("hi " + str(price))
