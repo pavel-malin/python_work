6e0df658c86c3b860d315df47db0fb634421e0ba
malinroot <kurchevskijpavel@gmail.com>
2019-03-25 14:57:26 +0300

    Add files via upload

---
+++ cars_if.py
@@ -0,0 +1,7 @@
+# If included in the list with a change
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
