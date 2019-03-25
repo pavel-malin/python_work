9f52922ca8ead7ac36ae6850de29144c62cd00c9
malinroot <kurchevskijpavel@gmail.com>
2019-03-25 14:58:00 +0300

    Add files via upload

---
+++ cars_not.py
@@ -0,0 +1,68 @@
+# Check whether the list(subaru, audi)
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+# Check enters different lists(11)
+car = 'subaru'
+car_0 = 'audi'
+car_1 = 'bmw'
+car_2 = 'toyota'
+car_3 = 'nissan'
+car_4 = 'lada'
+car_5 = 'subaru'
+car_6 = 'audi'
+car_7 = 'bmw'
+car_8 = 'toyota'
+car_9 = 'nissan'
+car_10 = 'lada'
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+print("\nIs car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("Is car_0 == 'subaru'? I predict False.")
+print(car_0 == 'subaru')
+print("\nIs car_0 == 'audi'? I predict True.")
+print(car_0 == 'audi')
+print("Is car_1 == 'nissan'? I predict False.")
+print(car_1 == 'nissan')
+print("\nIs car_1 == 'bmw'? I predict True.")
+print(car_1 == 'bmw')
+print("Is car_2 == 'nissan'? I predict False.")
+print(car_2 == 'nissan')
+print("\nIs car_2 == 'toyota'? I predict True.")
+print(car_2 == 'toyota')
+print("Is car_3 == 'bmw'? I predict False.")
+print(car_3 == 'bmw')
+print("\nIs car_3 == 'nissan'? I predict True.")
+print(car_3 == 'nissan')
+print("Is car_4 == 'nissan'? I predict False.")
+print(car_4 == 'nissan')
+print("\nIs car_4 == 'lada'? I predict True.")
+print(car_4 == 'lada')
+print("Is car_5 == 'audi'? I predict False.")
+print(car_5 == 'audi')
+print("\nIs car_5 == 'subaru'? I predict True.")
+print(car_5 == 'subaru')
+print("Is car_6 == 'bmw'? I predict False.")
+print(car_6 == 'bmw')
+print("\nIs car_6 == 'audi'? I predict True.")
+print(car_6 == 'audi')
+print("Is car_7 == 'nissan'? I predict False.")
+print(car_7 == 'nissan')
+print("\nIs car_7 == 'bmw'? I predict True.")
+print(car_7 == 'bmw')
+print("Is car_8 == 'audi'? I predict False.")
+print(car_8 == 'audi')
+print("\nIs car_8 == 'toyota'? I predict True.")
+print(car_8 == 'toyota')
+print("Is car_9 == 'toyota'? I predict False.")
+print(car_9 == 'toyota')
+print("\nIs car_9 == 'nissan'? I predict True.")
+print(car_9 == 'nissan')
+print("Is car_10 == 'bmw'? I predict False.")
+print(car_10 == 'bmw')
+print("\nIs car_10 == 'lada'? I predict True.")
+print(car_10 == 'lada')
