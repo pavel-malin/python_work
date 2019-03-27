2840584f09c79e182ac30a324148bb6a5c8c10ba
malinroot <kurchevskijpavel@gmail.com>
2019-03-27 17:27:09 +0300

    Add files via upload

---
+++ squares.py
@@ -0,0 +1,17 @@
+# 1-10 exponentiation and addition to the list
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
+
+# Code abbreviation
+squares = []
+for value in range(1,11):
+    squares.append(value**2)
+
+print(squares)
+
+# Code abbreviation
+suqares = [value**2 for value in range(1,11)]
+print(suqares)
