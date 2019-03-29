451e5d50df87d1ee9ea83dcbcfc6837bd1a13fc0
malinroot <kurchevskijpavel@gmail.com>
2019-03-29 12:13:55 +0300

    Add files via upload

---
+++ swiss_table.py
@@ -0,0 +1,13 @@
+# swiss table (tuple)
+swiss_table = ('adjika', 'adobo', 'azu', 'aivar', 'ayntopf',
+                'aioli', 'akutak', 'alani', 'alcoholic whipeed cream', 'aliera')
+print("Menu:")
+for swiss_tables in swiss_table:
+    print(swiss_tables)
+
+# Swiss table new
+swiss_table = ('adjika', 'adobo', 'azu', 'aivar')
+print("\nNew swiss table menu:")
+for swiss_tables in swiss_table:
+    print(swiss_tables)
+
