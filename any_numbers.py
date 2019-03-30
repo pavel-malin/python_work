721f9f3a71268b0b53d29fa75e9edb315587e434
malinroot <kurchevskijpavel@gmail.com>
2019-03-30 17:54:15 +0300

    Add files via upload

---
+++ any_numbers.py
@@ -0,0 +1,12 @@
+# Favorite number of friends with the conclusion
+# if the numbers
+any_numbers = {
+    'Bob': '6',
+    'Tedi': '25',
+    'Finar': '13',
+    'Any': '93',
+    }
+print("Favorite number Bob: " + any_numbers['Bob'] +
+    "\n" + "Favorite number Tedi: " + any_numbers['Tedi'] +
+    "\n" + "Favorite number Finar: " + any_numbers['Finar'] +
+    "\n" + "Favorite number Any: " + any_numbers['Any'])
