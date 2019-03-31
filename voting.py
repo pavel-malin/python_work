5ff0cb2a38cccadb22b21c53e80382049ec06968
malinroot <kurchevskijpavel@gmail.com>
2019-03-31 16:25:17 +0300

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,14 @@
+# # Matching cycles for conclusions by age of amounts
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+# # Matching cycles for conclusions by age of amounts
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
