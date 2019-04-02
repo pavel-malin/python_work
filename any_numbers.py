e071bc42fbafc130a58782cd47359c30c78793d5
malinroot <kurchevskijpavel@gmail.com>
2019-04-02 15:11:23 +0300

    Add files via upload

--- any_numbers.py
+++ any_numbers.py
@@ -10,3 +10,29 @@ print("Favorite number Bob: " + any_numbers['Bob'] +
     "\n" + "Favorite number Tedi: " + any_numbers['Tedi'] +
     "\n" + "Favorite number Finar: " + any_numbers['Finar'] +
     "\n" + "Favorite number Any: " + any_numbers['Any'])
+
+# Simplifying the top example using a dictionary and
+# adding favorite numbers of people
+any_numbers = {
+    'bob': {
+        'number': '6',
+        'numbers': '11',
+        },
+    'Tedi': {
+        'number': '25',
+        'numbers': '22',
+        },
+    'Finar': {
+        'number': '13',
+        'numbers': '3',
+        },
+    'Any': {
+        'number': '93',
+        'numbers': '3',
+        },
+    }
+
+for name, number_info in any_numbers.items():
+    print("\nFavorite number: " + name.title())
+    number = number_info['number'] + ", " + number_info['numbers']
+    print("\tNumbers: " + number)
