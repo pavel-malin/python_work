1486a3e1b32421f9ecb995af3997f83a0be60ede
malinroot <kurchevskijpavel@gmail.com>
2019-04-05 11:49:22 +0300

    Add files via upload

---
+++ citties.py
@@ -0,0 +1,12 @@
+# Executing a loop by value is correct and
+# completing through break
+prompt = "\nPlease enter the name of a city you have visited: "
+prompt += "\n(Enter 'quit' when you are finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
