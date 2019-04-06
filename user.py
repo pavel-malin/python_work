fe35cbb3441c2b2772c9d5fbca194253ca918e27
malinroot <kurchevskijpavel@gmail.com>
2019-04-07 00:41:12 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,16 @@
+# Output of the dictionary by
+# individual names
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+# Abbreviation for vocabulary
+for k, v in user_0.items():
+    print("\nKey: " + k)
+    print("Value: " + v)
