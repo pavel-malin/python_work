2895caafa252e45e2af3c2bc2386efa853a56897
malinroot <kurchevskijpavel@gmail.com>
2019-04-12 23:31:48 +0300

    Add files via upload

---
+++ my_car.py
@@ -0,0 +1,13 @@
+from electric_car import Car, ElectricCar
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_desctiptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+my_beetle = Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_desctiptive_name())
+
+my_tesla = ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_desctiptive_name())
