d83d52dd3ac332bfb1624ead2ef74de5388ba76f
malinroot <kurchevskijpavel@gmail.com>
2019-04-14 08:41:25 +0300

    Add files via upload

---
+++ cittes.py
@@ -0,0 +1,7 @@
+def describe_city(city='reykjavik', country='iceland'):
+    print(city.title() + " is in " + country.title())
+
+describe_city()
+describe_city(city='moscow', country='russia')
+describe_city(city='minsk', country='belarus')
+describe_city(city='warsaw', country='poland')
